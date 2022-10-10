a31dc9ab1e032274003a467e0092104981596928
Samule Maiyo <46650368+SamuelMaiyo@users.noreply.github.com>
2022-10-10 15:10:43 +0300

    First Commit

---
+++ backend/src/helpers/todosAcess.ts
@@ -0,0 +1,110 @@
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { createLogger } from '../utils/logger'
+import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate';
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const logger = createLogger('TodosAccess')
+
+// TODO: Implement the dataLayer logic
+
+export class TodosAccess {
+
+    constructor(
+      private readonly docClient: DocumentClient = createDynamoDBClient(),
+      private readonly todosTable = process.env.TODOS_TABLE) {
+    }
+  
+    async getTodosForUser(userId: string): Promise<TodoItem[]> {
+      logger.info('Getting all todos for the logged in user')
+  
+      const result = await this.docClient.scan({
+        TableName: this.todosTable,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+          ':userId': {'S': userId}
+        },
+      }).promise()
+  
+      const items = result.Items
+      return items as TodoItem[]
+    }
+  
+    async createTodo(todo: TodoItem): Promise<TodoItem> {
+  
+      logger.info('Creating a new todo item')
+  
+      await this.docClient.put({
+        TableName: this.todosTable,
+        Item: todo
+      }).promise()
+  
+      return todo
+    }
+  
+    async updateTodo(userId: string, todoId: string, todo: TodoUpdate): Promise<TodoItem> {
+  
+      logger.info('Updating an existing todo item')
+  
+      const updatedTodo: TodoItem = await this.docClient.update({
+        TableName: this.todosTable,
+        Key: {
+          userId: userId,
+          todoId: todoId
+        },
+        UpdateExpression: 'set #name = :v_name, #dueDate = :v_dueDate, #done = :v_done',
+        ExpressionAttributeNames: {
+          '#name': 'name',
+          '#dueDate': 'dueDate',
+          '#done': 'done'
+        },
+        ExpressionAttributeValues: {
+          ':v_name': todo.name,
+          ':v_dueDate': todo.dueDate,
+          ':v_done' : todo.done
+        },
+        ReturnValues: "ALL_NEW"
+      }).promise()
+  
+      return updatedTodo
+    }
+  
+    async updateAttachmentUrl(userId: string, todoId: string): Promise<void> {
+      await this.docClient.update({
+        TableName: this.todosTable,
+        Key: {
+          "userId": userId,
+          "todoId": todoId
+        },
+        UpdateExpression: "set attachmentUrl=:attachmentUrl",
+        ExpressionAttributeValues:{
+            ":attachmentUrl": `https://${this.bucketName}.s3.amazonaws.com/${todoId}`
+        }
+      }).promise()
+    }
+  
+    async deleteTodo(userId: string, todoId: string): Promise<void> {
+      await this.docClient.delete({
+        TableName: this.todosTable,
+        Key: {
+          "userId": userId,
+          "todoId": todoId
+        }
+      }).promise()
+    }
+  }
+  
+  function createDynamoDBClient() {
+      if (process.env.IS_OFFLINE) {
+        console.log('Creating a local DynamoDB instance')
+        return new XAWS.DynamoDB.DocumentClient({
+          region: 'localhost',
+          endpoint: 'http://localhost:8000'
+        })
+      }
+    
+      return new XAWS.DynamoDB.DocumentClient()
+    }
