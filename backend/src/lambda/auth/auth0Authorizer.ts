56357d958d7aa628e9a2ffca7f635ab7955bb52d
Samule Maiyo <46650368+SamuelMaiyo@users.noreply.github.com>
2022-10-30 16:27:13 +0300

    Additional Changes

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,7 +12,7 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'https://dev-qmysz9rx.us.auth0.com/.well-known/jwks.json'
+//const jwksUrl = 'https://dev-qmysz9rx.us.auth0.com/.well-known/jwks.json'
 const auth0secret = process.env.AUTH_0_SECRET
 
 export const handler = async (
@@ -57,7 +57,7 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+ // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
